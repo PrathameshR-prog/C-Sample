3804a9c86717e66d9c9ed247d19a683d3c57d70e
Prathamesh <pranshur@axxess.com>
2022-08-01 12:35:35 +0530

    Neww

---
+++ MyClass.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSharpSample
+{
+    internal class MyClass
+    {
+        internal  void Status()
+        {
+            Console.WriteLine("hi");
+            Console.WriteLine(1);
+            //k
+        }
+    }
+}
